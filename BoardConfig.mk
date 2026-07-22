e73d0edb31e5b2a1c34afd01339e24ce50fa5470
rdndds <rdndds@gmail.com>
2026-07-22 18:06:41 +0700

    try to use mt8781 instead of mt6789

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,7 +105,7 @@ BOARD_USES_VENDOR_DLKMIMAGE := true
 BOARD_EROFS_COMPRESS_HINTS := $(CONFIGS_PATH)/erofs_compress_hints.txt
 
 # Platform
-TARGET_BOARD_PLATFORM := mt6789
+TARGET_BOARD_PLATFORM := mt8781
 
 # Allow duplicate sysprop assignments
 BUILD_BROKEN_DUP_SYSPROP := true
--- device.mk
+++ device.mk
@@ -503,7 +503,7 @@ $(call soong_config_set_bool,android_hardware_mediatek_usb,audio_accessory_suppo
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek \
-    init.mt6789.usb.rc
+    init.mt8781.usb.rc
 
 # userdata
 PRODUCT_FS_COMPRESSION := 1
