35b9de01615207b481ff20651252d2bf6ca65dca
rdndds <rdndds@gmail.com>
2026-07-24 23:21:23 +0700

    add fenrir patch fastbootd bypass

--- device.mk
+++ device.mk
@@ -187,6 +187,9 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot-service.example_recovery \
     fastbootd:64
 
+# Fenrir
+$(call soong_config_set_bool,fastbootd,bypass_lock_state,true)
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
